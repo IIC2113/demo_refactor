abed3061755e0cb78ddbe0e2e5b6abb8ecc5ccb1
Antonio Ossa Guerra <aaossa@uc.cl>
2022-05-01 13:01:03 -0400

    :recycle: Move total_renter_points to movie types
    Instead of calculating the points for each rental in the Rental class,
    let each MovieType define how many points to provide (use 1 as default)
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- movie_type.rb
+++ movie_type.rb
@@ -1,13 +1,17 @@
 # Module with methods that must be implemented by MovieTypes
 module MovieType
-  def self.total_amount
+  def total_amount
     raise NotImplementedError
+  end
+
+  def total_renter_points(_)
+    1
   end
 end
 
 # Type for common movies (can be considered default)
 class RegularMovieType
-  include MovieType
+  extend MovieType
 
   def self.total_amount(days_rented)
     2 + (days_rented > 2 ? (days_rented - 2) * 1.5 : 0)
@@ -16,16 +20,21 @@ end
 
 # Movies that were recently released
 class NewReleaseMovieType
-  include MovieType
+  extend MovieType
 
   def self.total_amount(days_rented)
     days_rented * 3
+  end
+
+  def self.total_renter_points(days_rented)
+    base_renter_points = super
+    base_renter_points + (days_rented > 1 ? 1 : 0)
   end
 end
 
 # Movies for children
 class ChildrensMovieType
-  include MovieType
+  extend MovieType
 
   def self.total_amount(days_rented)
     1.5 + (days_rented > 3 ? (days_rented - 3) * 1.5 : 0)
--- rental.rb
+++ rental.rb
@@ -12,10 +12,6 @@ class Rental
   end
 
   def total_renter_points
-    frequent_renter_points = 1
-    if @movie.movie_type == NewReleaseMovieType && @days_rented > 1
-        frequent_renter_points += 1
-    end
-    frequent_renter_points
+    @movie.movie_type.total_renter_points(@days_rented)
   end
 end
