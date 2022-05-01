0333da752a58a5c8b2767d423b29c4b253814537
Antonio Ossa Guerra <aaossa@uc.cl>
2022-05-01 13:22:38 -0400

    :recycle: Use the correct quotation on strings
    Following Rubocop recommendations about when to use single quotes or
    double quotes
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- rental_test.rb
+++ rental_test.rb
@@ -4,18 +4,22 @@ require_relative 'rental'
 require_relative 'customer'
 require 'minitest/autorun'
 
-describe "Movie rentals" do
+describe 'Movie rentals' do
   def setup
-    @c = Customer.new("Jane")
-    @m1 = Movie.new("1984", RegularMovieType)
-    @m2 = Movie.new("Platoon", NewReleaseMovieType)
+    @c = Customer.new('Jane')
+    @m1 = Movie.new('1984', RegularMovieType)
+    @m2 = Movie.new('Platoon', NewReleaseMovieType)
     @r1 = Rental.new(@m1, 2)
     @r2 = Rental.new(@m2, 3)
     @c.add_rental(@r1)
     @c.add_rental(@r2)
   end
 
-  it "correctly does statement" do
-    _(@c.statement).must_equal("Rental Record for Jane\n\t1984\t2\n\tPlatoon\t9\nAmount owed is 11\nYou earned 3 frequent renter points")
+  it 'correctly does statement' do
+    _(@c.statement).must_equal(
+      "Rental Record for Jane\n" \
+      "\t1984\t2\n\tPlatoon\t9\nAmount owed is 11\n" \
+      'You earned 3 frequent renter points'
+    )
   end
 end
